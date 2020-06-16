93f544e2a212d200c3b3b7974bd45e4feb74a5ff
Ikramuddin21 <shaonakhand2122@gmail.com>
2020-06-16 12:30:51 +0600

    update node- mongo site

--- index.js
+++ index.js
@@ -2,25 +2,33 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const dbUser = 'dbUser';
-const pass = 'EgM64zuLTAwVgJcF';
-const uri = "mongodb+srv://dbUser:EgM64zuLTAwVgJcF@cluster0-ilfiw.mongodb.net/onlineStore?retryWrites=true&w=majority";
+const uri = process.env.DB_PATH;
 
-const client = new MongoClient(uri, { useNewUrlParser: true });
+let client = new MongoClient(uri, { useNewUrlParser: true });
 const users = ['Asad', 'Moin', 'Sabed', 'Susmita', 'Sohana', 'Sabana'];
 
-app.get('/', (req, res) =>{
-    const fruit = {
-        product: 'ada',
-        price: 220
-    }
-    res.send(fruit);
+app.get('/products', (req, res) =>{
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        collection.find({name:'Mobile'}).limit(10).toArray((err, documents)=>{
+            if(err){
+                console.log(err)
+                res.status(500).send({message:err});
+            }
+            else{
+                res.send(documents);
+            }
+        });
+        client.close();
+      });
 });
 
 app.get('/users/:id', (req, res) =>{
@@ -32,15 +40,20 @@ app.get('/users/:id', (req, res) =>{
 //post
 app.post('/addProduct', (req, res) => {
     const product = req.body;
-    console.log('Testing', product);
-
+    client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
         const collection = client.db("onlineStore").collection("products");
-        collection.insertOne(product, (err, res)=>{
-            console.log('Successfully inserted', res)
-            // res.send(product);
+        collection.insertOne(product, (err, result)=>{
+            if(err){
+                console.log(err)
+                res.status(500).send({message:err});
+            }
+            else{
+                res.send(result.ops[0]);
+            }
         });
         client.close();
       });
 });
-app.listen(4000, () => console.log('Listening to port 4000'));
+const port = process.env.PORT || 4000
+app.listen(port, () => console.log('Listening to port 4000'));
